6e991b5f248aa0fb6d2957c5daf2cea552f080f8
Francis <fgoulet@ixmedia.com>
2017-01-18 09:59:07 -0500

    Remove ActionView dep.

--- lib/ix_social/actionview.rb
+++
@@ -1,5 +0,0 @@
-require 'action_view'
-require 'action_view/context'
-
-::IxSocial::Helpers::Facebook.send :include, ::ActionView::Context
-::IxSocial::Helpers::Instagram.send :include, ::ActionView::Context
