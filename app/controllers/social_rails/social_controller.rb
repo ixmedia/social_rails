ad979fd7d2f6aa41168e6440f159b7c64590fbbb
Francis G <francis@ixmedia.com>
2018-01-30 08:52:55 -0500

    Fix gem application controller heritage

--- app/controllers/social_rails/social_controller.rb
+++ app/controllers/social_rails/social_controller.rb
@@ -1,7 +1,7 @@
 require_dependency "social_rails/application_controller"
 
 module SocialRails
-  class SocialController < ApplicationController
+  class SocialController < SocialRails::ApplicationController
     def latest
       render ["social_rails", params[:template], "latest"].compact.join("/"), layout: false
     end
